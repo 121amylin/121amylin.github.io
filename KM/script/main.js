8da87f67c7b357bc340c78838e6950b42e7e963b
121amylin <121amylin@gmail.com>
2021-07-11 21:49:13 +0800

    add light Box effect  __ scroll  possition

--- KM/script/main.js
+++ KM/script/main.js
@@ -2,7 +2,7 @@ let alert_tit = document.querySelectorAll(".alert_tit");
 let alert_content_box=document.querySelectorAll('.alert_content_box')
 let close = document.querySelectorAll(".close");
 let elemBody=document.querySelector('body')
-
+let nowH=''
 function closeAll(){
   alert_content_box.forEach(item=>{
     item.classList.remove('show')
@@ -18,6 +18,7 @@ alert_tit.forEach(item => {
     let childen=this.nextElementSibling.querySelector(".alert_content")
     // let h =window.pageYOffset + (window.screen.height - childen.clientHeight)/2;
     let h = (window.screen.height/2) - childen.clientHeight-80;
+    nowH = window.pageYOffset;
     this.nextElementSibling.classList.remove("hide");
     this.nextElementSibling.classList.add("show");
     childen.style.top = h + "px";
@@ -28,6 +29,7 @@ alert_tit.forEach(item => {
 close.forEach(item => {
   item.addEventListener('click',()=>{
     closeAll()
+    window.document.documentElement.scrollTop = nowH;
+    nowH=''
   })
-  console.log(1)
 });
