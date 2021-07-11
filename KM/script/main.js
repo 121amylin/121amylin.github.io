1fd607437cd033f814f506f5c06da24707bf745c
121amylin <121amylin@gmail.com>
2021-07-11 21:34:03 +0800

    add light Box effect

---
+++ KM/script/main.js
@@ -0,0 +1,33 @@
+let alert_tit = document.querySelectorAll(".alert_tit");
+let alert_content_box=document.querySelectorAll('.alert_content_box')
+let close = document.querySelectorAll(".close");
+let elemBody=document.querySelector('body')
+
+function closeAll(){
+  alert_content_box.forEach(item=>{
+    item.classList.remove('show')
+    item.classList.add('hide')
+    elemBody.classList.remove("fixed");
+  })
+}
+
+closeAll()
+
+alert_tit.forEach(item => {
+  item.addEventListener("click", function () {
+    let childen=this.nextElementSibling.querySelector(".alert_content")
+    // let h =window.pageYOffset + (window.screen.height - childen.clientHeight)/2;
+    let h = (window.screen.height/2) - childen.clientHeight-80;
+    this.nextElementSibling.classList.remove("hide");
+    this.nextElementSibling.classList.add("show");
+    childen.style.top = h + "px";
+    elemBody.classList.add("fixed");
+  });
+});
+
+close.forEach(item => {
+  item.addEventListener('click',()=>{
+    closeAll()
+  })
+  console.log(1)
+});
